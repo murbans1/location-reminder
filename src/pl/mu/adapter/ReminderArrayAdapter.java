c08397c540304fc47b4ecb56736df0683af8dcd2
Maciek <maciek.urbanski1@gmail.com>
2012-06-22 09:24:59 +0200

    array adapter was added

---
+++ src/pl/mu/adapter/ReminderArrayAdapter.java
@@ -0,0 +1,55 @@
+package pl.mu.adapter;
+
+import java.util.List;
+
+import pl.mu.R;
+import pl.mu.data.ReminderObject;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ReminderArrayAdapter extends ArrayAdapter<ReminderObject> {
+	private Context context;
+	private List<ReminderObject> data;
+	
+	public ReminderArrayAdapter(Context context,List<ReminderObject> data) {
+		super(context, R.layout.list_item, data);   
+		this.context = context;
+        this.data = data;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+		ViewHolder viewHolder = null;
+
+		if(rowView == null){
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+			
+			Log.d("ReminderArrayAdapter", "convertView null");
+			rowView = inflater.inflate(R.layout.list_item, null);
+			
+			viewHolder = new ViewHolder();
+		    viewHolder.title = (TextView) rowView.findViewById(R.id.textView_title);
+			viewHolder.description =(TextView) rowView.findViewById(R.id.textView_description);
+		   
+		    convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) rowView.getTag();
+		} 
+		
+		viewHolder.title.setText(data.get(position).title);
+		viewHolder.description.setText(data.get(position).description);
+		
+		return rowView;
+	}
+	
+	static class ViewHolder{
+		TextView title;
+		TextView description;
+	}
+}
